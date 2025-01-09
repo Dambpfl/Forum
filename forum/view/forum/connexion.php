a7e72ef4bac70610b512d451fed654cd94e0300b
Damien <boenapfel.damien@yahoo.fr>
2025-01-09 09:15:42 +0100

    CSS Connexion->Inscription

--- forum/view/forum/connexion.php
+++ forum/view/forum/connexion.php
@@ -1,12 +1,16 @@
 <body>
-    <h1>Se connecter</h1>
-    <form class="connexion" action="index.php?ctrl=security&action=login" method="POST">
-        <label class="t-email" for="email">Email</label>
-        <input class="email" type="email" name="email" id="email"><br>
-
-        <label class="t-password" for="password">Mot de passe</label>
-        <input class="password" type="password" name="password" id="password"><br>
-
-        <input class="submit" type="submit" name="submit" value="Se connecter">
-    </form>
+    <h1>Connexion</h1>
+    <div class="container-connexion">
+        <form class="connexion" action="index.php?ctrl=security&action=login" method="POST">
+            
+            <label class="t-email" for="email">Email</label>
+            <input class="email" type="email" name="email" id="email"
+            placeholder="E-mail.."><br>
+    
+            <label class="t-password" for="password">Mot de passe</label>
+            <input class="password" type="password" name="password" id="password" placeholder="Mot de passe.."><br>
+    
+            <input class="submit" type="submit" name="submit" value="Se connecter">
+        </form>
+    </div>
 </body>
--- forum/view/forum/inscription.php
+++ forum/view/forum/inscription.php
@@ -1,18 +1,23 @@
 <body> 
-    <h1>S'inscrire</h1>
-    <form action="index.php?ctrl=security&action=register" method="POST">
-        <label for="pseudo">Pseudo</label>
-        <input type="text" name="pseudo" id="pseudo"><br>
+    <h1>Inscription</h1>
+    <div class="container-inscription">
 
-        <label for="email">Mail</label>
-        <input type="email" name="email" id="email"><br>
-
-        <label for="pass">Mot de passe</label>
-        <input type="password" name="pass1" id="pass1"><br>
-
-        <label for="pass2">Confirmation du mot de passe</label>
-        <input type="password" name="pass2" id="pass2"><br>
-
-        <input type="submit" name="submit" value="S'enregistrer">
-    </form>
+        <form class="inscription" action="index.php?ctrl=security&action=register" method="POST">
+            <label class="t-pseudo" for="pseudo">Pseudo :</label>
+            <input class="pseudo" type="text" name="pseudo" id="pseudo" placeholder="Pseudo.."><br>
+    
+            <label class="t-email" for="email">Mail :</label>
+            <input class="email" type="email" name="email" id="email"
+            placeholder="Email.."><br>
+    
+            <label class="t-password" for="pass">Mot de passe :</label>
+            <input class="password" type="password" name="pass1" id="pass1"
+            placeholder="Mot de passe.."><br>
+    
+            <label class="t-password" for="pass2">Confirmer le mot de passe :</label>
+            <input class="password" type="password" name="pass2" id="pass2" placeholder="Mot de passe.."><br>
+    
+            <input class="submit" type="submit" name="submit" value="Crée mon compte">
+        </form>
+    </div>
 </body>
