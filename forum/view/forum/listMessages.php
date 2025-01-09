7ef2b6b8aa1b037d4ca69aac212022577bd54f13
Damien <boenapfel.damien@yahoo.fr>
2025-01-09 15:05:03 +0100

    CSS -> zone de message

--- forum/view/forum/listMessages.php
+++ forum/view/forum/listMessages.php
@@ -3,8 +3,8 @@
     $messages = $result["data"]["messages"];
 ?>
 
-<a href="index.php?ctrl=forum&action=listSujetsByCategorie&id=<?= $sujet->getCategorie()->getId() ?>"><i class="fa-solid fa-arrow-left"> retour</i></a>
-<h3><?= $sujet->getTitre() ?></h3>
+<h1><?= $sujet->getTitre() ?></h1>
+<a class="retour" href="index.php?ctrl=forum&action=listSujetsByCategorie&id=<?= $sujet->getCategorie()->getId() ?>"><i class="fa-solid fa-arrow-left"> retour</i></a>
 
 
 <!-- Affiche les informations des messages -->
@@ -15,28 +15,29 @@
             <div class="message-date"><p>le <?= $message->getDateCreation() ?></p></div>
             <div class="message-text"><p><?= $message->getTexte() ?></a></p></div>
         </div>
+        
+        <!-- Si l'utilisateur à le meme pseudo que le message alors supprimer OU admin supp n'importe quel message -->
+        <?php if(App\Session::getUser() && $message->getUtilisateur()->getPseudo() === $_SESSION['user']->getPseudo() || App\Session::isAdmin()) { ?>
+            <a href="index.php?ctrl=forum&action=deleteMessage&id=<?= $message->getId() ?>">Supprimer</a>
+            <?php } ?>
+            <?php } ?>
 </div>
-
-<!-- Si l'utilisateur à le meme pseudo que le message alors supprimer OU admin supp n'importe quel message -->
-    <?php if(App\Session::getUser() && $message->getUtilisateur()->getPseudo() === $_SESSION['user']->getPseudo() || App\Session::isAdmin()) { ?>
-        <a href="index.php?ctrl=forum&action=deleteMessage&id=<?= $message->getId() ?>">Supprimer</a>
-    <?php } ?>
-<?php } ?>
-
-<!-- Si utilisateur et sujet non verrouiller -->
-<?php if(App\Session::getUser() && $sujet->getVerrouillage() === 0){ ?>
-
-<h1>Répondre</h1>
-
-    <form action="index.php?ctrl=forum&action=addMessage&id=<?= $sujet->getId() ?>" method="post">
-        <p>
-            <label for="pseudo"><?= $_SESSION['user'] ?> :<br><!-- A changer pour nom utilisateur connecter -->
-            <textarea name="texte" id="texte" placeholder="Tapez votre message ici.." rows="10" cols="50"></textarea>
-            </label>
-        </p>
-        <p>
-            <input type="submit" name="submit" value="Poster">
-        </p>
-      
-    </form>
-<?php } ?>
+            
+            <!-- Si utilisateur et sujet non verrouiller -->
+            <?php if(App\Session::getUser() && $sujet->getVerrouillage() === 0){ ?>
+                
+                <h1>Répondre</h1>
+                
+                <form action="index.php?ctrl=forum&action=addMessage&id=<?= $sujet->getId() ?>" method="post">
+                    <p>
+                        <label for="pseudo"><?= $_SESSION['user'] ?> :<br><!-- A changer pour nom utilisateur connecter -->
+                        <textarea name="texte" id="texte" placeholder="Tapez votre message ici.." rows="10" cols="50"></textarea>
+                    </label>
+                </p>
+                <p>
+                    <input type="submit" name="submit" value="Poster">
+                </p>
+                
+            </form>
+            <?php } ?>
+            
