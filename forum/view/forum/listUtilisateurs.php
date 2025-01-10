22ccce1669fe2b56deaea5b817f9f9f196c3511f
Damien <boenapfel.damien@yahoo.fr>
2025-01-10 09:47:04 +0100

    CSS -> listUtilisateurs

--- forum/view/forum/listUtilisateurs.php
+++ forum/view/forum/listUtilisateurs.php
@@ -5,7 +5,25 @@ $users = $result["data"]["users"];
 
 ?>
 
-<?php
-foreach($users as $user) {
-    echo $user->getPseudo();?> <br>
-<?php } ?>
+<h1>Liste des utilisateurs</h1>
+
+<div class="container-table">
+    <table>
+        <thead>
+            <tr>
+                <th class="id-table">ID</th>
+                <th class="pseudo-table">Pseudo</th>
+                <th class= "role-table">Rôle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($users as $user) { ?> 
+                <tr>
+                    <td class="id-table"><?php echo $user->getId();?> </td>
+                    <td class="pseudo-table"><?php echo $user->getPseudo();?> </td>
+                    <td class= "role-table"> <?php echo $user->getRole();?></td>
+                </tr>   
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
