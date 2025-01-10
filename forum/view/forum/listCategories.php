4a4e80e0688933c7e580847ef9f22209e07d5795
Damien <boenapfel.damien@yahoo.fr>
2025-01-10 14:48:54 +0100

    Fix : CSS

--- forum/view/forum/listCategories.php
+++ forum/view/forum/listCategories.php
@@ -6,7 +6,7 @@
 
 <div class="container-categorie">
     <?php foreach($categories as $categorie ){ ?>
-        <div class="wrapper-categorie">
+        <div class="categorie-wrapper">
             <p><a class="categorie" href="index.php?ctrl=forum&action=listSujetsByCategorie&id=<?= $categorie->getId() ?>"><?= $categorie->getNomCategorie() ?></a></p>  
         </div>
     <?php } ?>
